105b4131ee3f3a19900284a5efd4974299b47d15
giabao2807 <102190252@sv1.dut.udn.vn>
2022-11-05 17:59:58 +0700

    feat: adopt_request manage + email for action request

--- api_activity/serializers/adopt_request.py
+++ api_activity/serializers/adopt_request.py
@@ -11,13 +11,17 @@ class RegisterAdoptRequestSerializer(ModelSerializer):
 
 
 class AdoptRequestSerializer(ModelSerializer):
-    from api_activity.serializers import RegisterAdoptRequestDetailSerializer
     from api_children.serializers import ChildrenSerializer
 
-    adopt_request_detail = RegisterAdoptRequestDetailSerializer(read_only=True, required=False)
     children = ChildrenSerializer(read_only=True, required=False)
 
     class Meta:
         model = AdoptRequest
         fields = '__all__'
         ordering = ('created_at', 'updated_at')
+
+    def to_representation(self, instance):
+        ret = super().to_representation(instance)
+        ret["adopter_name"] = instance.adopt_request_detail.adopter.name
+        ret["approver"] = instance.approver.name
+        return ret
